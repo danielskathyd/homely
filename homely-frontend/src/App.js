55a9f3530f48ced2125454de44de766e63aeb72a
jay <jongyunjaypark@gmail.com>
2020-03-28 12:45:45 -0700

    grids

--- homely-frontend/src/App.js
+++ homely-frontend/src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import logo from "./logo.svg";
 import "./App.css";
 import { Button } from "@material-ui/core";
-import axios from "axios"
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import axios from "axios";
+import { AppBar } from "./components/AppBar";
+import Todo from "./components/Todo";
 
-import { AppBar } from "./components/AppBar"
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  }
+}));
+const classes = useStyles();
 
 class App extends React.Component {
   constructor(props) {
@@ -21,35 +37,55 @@ class App extends React.Component {
   // Right now, you're always me
   fetchUserInfo() {
     axios
-          .get("http://localhost:8000/api/users/")
-          .then(res => this.setState({
-            activeUser: res.data[0],
-            activeUserTodos: res.data[0]['todo_set']
-          }))
-          .catch(err => console.log(err));
+      .get("http://localhost:8000/api/users/")
+      .then(res =>
+        this.setState({
+          activeUser: res.data[0],
+          activeUserTodos: res.data[0]["todo_set"]
+        })
+      )
+      .catch(err => console.log(err));
   }
   renderTodos() {
-    if(!this.state.activeUserTodos) return;
+    if (!this.state.activeUserTodos) return;
     let todoList = [];
-    for(let todo of this.state.activeUserTodos) {
-      todoList.push(
-        <li key={todo.id}>
-          {todo.title}
-        </li>
-      )
+    for (let todo of this.state.activeUserTodos) {
+      todoList.push(<li key={todo.id}>{todo.title}</li>);
     }
     return todoList;
   }
   render() {
     console.log(this.state.activeUserTodos);
-    let activeUserName = this.state.activeUser ? this.state.activeUser.username : "";
+    let activeUserName = this.state.activeUser
+      ? this.state.activeUser.username
+      : "";
     return (
-      <div className="App">
-        <Button color="primary">Hello {activeUserName}</Button>
-        <h2>Here are your daily todos:</h2>
-        <ol>
-          {this.renderTodos()}
-        </ol>
+      <div>
+        <div className={classes.root}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Button color="primary">Hello {activeUserName}</Button>
+            </Grid>
+            <Grid item xs={6}>
+              <Todo></Todo>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper className={classes.paper}>xs=6</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <h2>Here are your daily todos:</h2>
+            </Grid>
+            <Grid item xs={3}>
+              <ol>{this.renderTodos()}</ol>
+            </Grid>
+            <Grid item xs={3}>
+              <p>hi</p>
+            </Grid>
+            <Grid item xs={3}>
+              <p>hi</p>
+            </Grid>
+          </Grid>
+        </div>
       </div>
     );
   }
