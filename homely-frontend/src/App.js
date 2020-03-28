b2ed955b6e7c7ddc55d465a94132c4cce117a471
jay <jongyunjaypark@gmail.com>
2020-03-28 00:08:19 -0700

    breaking things

--- homely-frontend/src/App.js
+++ homely-frontend/src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import AppBar from "./components/AppBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <AppBar></AppBar>
     </div>
   );
 }
---
+++ homely-frontend/src/components/AppBar/AppBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+
+export default class AppBar extends React.component {
+  render() {
+    return <Button color="primary">Hello World</Button>;
+  }
+}
